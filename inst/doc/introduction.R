35dfce38173040c37100c13cdb012a64b34756f3
Bob Rudis <bob@rudis.net>
2015-03-09 12:37:21 -0400

    vignette & readme

--- inst/doc/introduction.R
+++ inst/doc/introduction.R
@@ -1,7 +1,7 @@
 ## ----setup, echo = FALSE, message = FALSE--------------------------------
 knitr::opts_chunk$set(collapse = TRUE, comment = "#>")
 options(dplyr.print_min = 4L, dplyr.print_max = 4L)
-library(streamgraph)
+library(ipapi)
 
 ## ----geo1, message=FALSE-------------------------------------------------
 geolocate(c(NA, "10.0.1.1", "", "72.33.67.89", "dds.ec", " ", "search.twitter.com"), 
